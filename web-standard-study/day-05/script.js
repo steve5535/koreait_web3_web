dc6199e77c2b8bcfa8fe00b7b55414d7f23331a0
steve5535 <steve5535@naver.com>
2026-06-22 21:49:54 +0900

    jave script if문

---
+++ web-standard-study/day-05/script.js
@@ -0,0 +1,28 @@
+let num = 10;
+
+if (num % 2 === 0) {
+    console.log("짝수입니다")
+} else {
+    console.log("홀수입니다.")
+}
+
+let km = 5;
+if (km < 2) {
+    console.log("걸어가기")
+} else if (km < 5) {
+    console.log("택시 타기")
+} else if (km >= 5) {
+    console.log("기차 타기")
+}
+
+const score = 60;
+
+if (score >= 90) {
+    console.log("A")
+} else if (score >= 80) {
+    console.log("B")
+} else if (score >= 70) {
+    console.log("C")
+} else {
+    console.log("재시험")
+}
